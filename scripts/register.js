e18846e267dfd3e6a087734a845525f987bbe1e6
regina nhyira dadzie <reginadadzie60@gmail.com>
2024-09-16 11:53:13 +0000

    registration login using JavaScript

---
+++ scripts/register.js
@@ -0,0 +1,10 @@
+// Select form by id
+const registerform = document.getElementById('register-form');
+// Handle form submit event
+registerForm.onsubmit=function() {
+      // Collect username
+      // Collect email
+      // Collect password
+      // Save user information
+      // Send confirmation email
+}
